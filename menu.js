fcc46f4b29e2de87e9dfbf66b96aa4ceddc6f080
jonahmaligaya <maligayajonah@gmail.com>
2024-06-15 21:48:52 -0400

    Polished consumer landing page prototype 1. Added prototype for sponsor landing page, unfinished.

--- menu.js
+++ menu.js
@@ -1,7 +1,7 @@
 const menuButton = document.querySelector(".menu_button");
 const menu = document.querySelector(".menu");
 const menuList = document.querySelector(".menu_list");
-const consumer = document.querySelector(".consumer");
+const content = document.querySelector(".content");
 let openMenu = false;
 menuButton.addEventListener("click", toggleMenu);
 function toggleMenu() {
@@ -9,14 +9,14 @@ function toggleMenu() {
         menuButton.classList.add("close");
         menu.classList.add("open");
         menuList.classList.add("open");
-        consumer.classList.add("close");
+        content.classList.add("close");
         
         openMenu = true;
     } else {
         menuButton.classList.remove("close");
         menu.classList.remove("open");
-        consumer.classList.remove("close");
         menuList.classList.remove("open");
+        content.classList.remove("close");
 
         openMenu = false;
     }
