6f65413f1d8aa70562b3a21f37427586ca54a72e
jonahmaligaya <maligayajonah@gmail.com>
2024-06-17 19:23:38 -0400

    First prototype of sponsor page, with placeholder art, no direct links as of right now.

--- menu.js
+++ menu.js
@@ -2,10 +2,14 @@ const menuButton = document.querySelector(".menu_button");
 const menu = document.querySelector(".menu");
 const menuList = document.querySelector(".menu_list");
 const content = document.querySelector(".content");
+const html = document.querySelector("html");
+const body = document.querySelector("body");
 let openMenu = false;
 menuButton.addEventListener("click", toggleMenu);
 function toggleMenu() {
     if (!openMenu) {
+        html.classList.add("unscrollable");
+        body.classList.add("unscrollable");
         menuButton.classList.add("close");
         menu.classList.add("open");
         menuList.classList.add("open");
@@ -13,6 +17,8 @@ function toggleMenu() {
         
         openMenu = true;
     } else {
+        html.classList.remove("unscrollable");
+        body.classList.remove("unscrollable");
         menuButton.classList.remove("close");
         menu.classList.remove("open");
         menuList.classList.remove("open");
